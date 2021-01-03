31ab52c08ee19716b378195950f5ce31852b6dcd
George Madrid <gmadrid@gmail.com>
2021-01-02 22:42:45 -0500

    Adding some NOTES and docs.

--- libs/grids/src/gridtrait.rs
+++ libs/grids/src/gridtrait.rs
@@ -9,6 +9,7 @@ pub enum GridId {
     SmallOne,
     SmallTwo,
     Combined,
+    Flipped,
 }
 
 #[derive(Clone, Copy, Debug, Deserialize, Serialize)]
@@ -27,8 +28,18 @@ impl<'a> CellId {
 // A Grid is essentially a matrix of Color values.
 // It represents a knitting pattern.
 // It also include a facility for naming grids.
+/// `GridTrait` is the base abstraction in the 'grids' crate. It represents a grid of cells.
+///
+/// A struct that implements `GridTrait` has the following:
+/// * an id, represented by [GridId]
+/// * a rectangular matrix, each cell of which has:
+///   * a [Color]
+///   * an id, represented by a [CellId]. If an implementation of GridTrait transforms an
+///     underlying grid, the cell ids are those of the base grid.
 pub trait GridTrait {
+    /// Returns the [GridId] associated with this grid.
     fn grid_id(&self) -> GridId;
+
     fn cell_id(&self, row: usize, col: usize) -> CellId {
         CellId::new(self.grid_id(), row, col)
     }
